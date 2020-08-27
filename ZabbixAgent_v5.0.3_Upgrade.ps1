03a0142fb13cd5f2ac24a2bd9fbf4bd8b20d60eb
Ashokcbe1 <52443308+Ashokcbe1@users.noreply.github.com>
2020-08-28 01:40:52 +0530

    Update ZabbixAgent_v5.0.3_Upgrade.ps1

--- ZabbixAgent_v5.0.3_Upgrade.ps1
+++ ZabbixAgent_v5.0.3_Upgrade.ps1
@@ -17,11 +17,11 @@ $serverHostname =  Invoke-Command -ScriptBlock {hostname}
 mkdir "C:\Program Files\Zabbix-Agent"
 
 # Unzipping file to c:\zabbix
-Expand-Archive "\\4D-VM05\Software\zabbix_agent-5.0.3-windows-amd64-openssl.zip" "c:\Program Files\Zabbix-Agent"  
+Expand-Archive "\\system Host Name\shared folder\zabbix_agent-5.0.3-windows-amd64-openssl.zip" "c:\Program Files\Zabbix-Agent"  
 
 
 # Replaces 127.0.0.1 with your Zabbix server IP in the config file
-(Get-Content -Path "c:\Program Files\Zabbix-Agent\conf\zabbix_agentd.conf") | ForEach-Object {$_ -Replace '127.0.0.1', "172.16.1.76"} | Set-Content -Path "c:\Program Files\Zabbix-Agent\conf\zabbix_agentd.conf"
+(Get-Content -Path "c:\Program Files\Zabbix-Agent\conf\zabbix_agentd.conf") | ForEach-Object {$_ -Replace '127.0.0.1', "Zabbix Server IP"} | Set-Content -Path "c:\Program Files\Zabbix-Agent\conf\zabbix_agentd.conf"
 
 # Replaces hostname in the config file
 (Get-Content -Path "c:\Program Files\Zabbix-Agent\conf\zabbix_agentd.conf") | ForEach-Object {$_ -Replace 'Windows host', "$ServerHostname"} | Set-Content -Path "c:\Program Files\Zabbix-Agent\conf\zabbix_agentd.conf"
